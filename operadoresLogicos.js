b453195501838c9ff17355325e56003d806dcba7
Remy Neto <remyydev@gmail.com>
2025-02-12 21:44:45 -0300

    operadores - sem comment

--- operadoresLogicos.js
+++ operadoresLogicos.js
@@ -1,80 +1,79 @@
 // // && - and |  || - or | ! - not
 
-// let n1 = 10, n2 = 5, n3 = 15, n4 = 2;
+let n1 = 10, n2 = 5, n3 = 15, n4 = 2;
 
-// console.log(n1 > n2 && n1 >= n2)
+console.log(n1 > n2 && n1 >= n2)
 
-// // Desafio Operadores Logicos - 1
+// Desafio Operadores Logicos - 1
 
-// let num1 = 12, num2 = 8;
+let num1 = 12, num2 = 8;
 
-// console.log(num1 > num2 && num1 != num2);
+console.log(num1 > num2 && num1 != num2);
 
-// // Desafio Operadores Logicos - 2
+// Desafio Operadores Logicos - 2
 
-// let valor1 = false, valor2 = true;
+let valor1 = false, valor2 = true;
 
-// console.log(valor1 || valor2);
+console.log(valor1 || valor2);
 
-// // Desafio Operadores Logicos - 3
+// Desafio Operadores Logicos - 3
 
-// let a = 10, b = 5, c = 8, d = 12;
+let a = 10, b = 5, c = 8, d = 12;
 
-// console.log(a > b && c < d);
-// console.log(a < b || c > d);
-// console.log(!(a > b))
+console.log(a > b && c < d);
+console.log(a < b || c > d);
+console.log(!(a > b))
 
-// // Operadores BitWise
-// // & | ^ >> << 
-// let valorN1 = 22, valorN2 = 12;
+// Operadores BitWise
+// & | ^ >> << 
 
-// let res = valorN1 >> 2
+let valorN1 = 22, valorN2 = 12;
 
-// console.log(res);
+let res = valorN1 >> 2
 
-// // Pré / Pós Incremento
+console.log(res);
 
-// let n = 10;
+// Pré / Pós Incremento
 
-// console.log(++n);
+let n = 10;
 
-// console.log(n);
+console.log(++n);
 
-// // Operador Térnario
+console.log(n);
 
-// n  =  10;
+// Operador Térnario
 
-// let resultado = (!(n%2) ? "Par": "Impar");
-// console.log(resultado);
+n  =  10;
 
-// // Operador Typeof
+let resultado = (!(n%2) ? "Par": "Impar");
+console.log(resultado);
 
-// let v1 = 10, v2= "10", v3 = v1 === v2, v4 = {nome: "Remy"};
+// Operador Typeof
 
-// console.log("Valor " + v1 + " - Tipo: " + typeof(v1));
-// console.log("Valor " + v2 + " - Tipo: " + typeof(v2));
-// console.log("Valor " + v3 + " - Tipo: " + typeof(v3));
-// console.log("Valor " + v4 + " - Tipo: " + typeof(v4));
+let v1 = 10, v2= "10", v3 = v1 === v2, v4 = {nome: "Remy"};
 
-// // Operador Spread
+console.log("Valor " + v1 + " - Tipo: " + typeof(v1));
+console.log("Valor " + v2 + " - Tipo: " + typeof(v2));
+console.log("Valor " + v3 + " - Tipo: " + typeof(v3));
+console.log("Valor " + v4 + " - Tipo: " + typeof(v4));
 
-// let b1 = [10,20,30];
-// let b2 = [11,22,33,55,77];
-// let b3 = [...b1,...b2]
+// Operador Spread
 
-// // console.log("b1: " + b1);
-// // console.log("b2: " + b2);
-// console.log("b3: " + b3);
+let b1 = [10,20,30];
+let b2 = [11,22,33,55,77];
+let b3 = [...b1,...b2]
 
-// const jogador1 = {nome: "Remy", energia: 100, vidas: 3, magia:150}
-// const jogador2 = {nome: "Remy2", energia: 95, vidas: 5, forca: 80}
-// const jogador3 = {...jogador1, ...jogador2}
+console.log("b1: " + b1);
+console.log("b2: " + b2);
+console.log("b3: " + b3);
 
-// console.log(jogador3)
+const jogador1 = {nome: "Remy", energia: 100, vidas: 3, magia:150}
+const jogador2 = {nome: "Remy2", energia: 95, vidas: 5, forca: 80}
+const jogador3 = {...jogador1, ...jogador2}
 
+console.log(jogador3)
 
-
-// // Desafio Operadores Bitwise 
+// Desafio Operadores Bitwise 
 
 let valorA = 15, valorB = 25, valorC = 20, valorD  = 35;
 
